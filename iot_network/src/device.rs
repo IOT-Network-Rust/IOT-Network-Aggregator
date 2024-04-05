8bbec11b0373c89c342ce5b54022e86b5d2ddd3d
Liam <liamisrael200@gmail.com>
2024-04-05 10:58:42 -0400

    Updated Code

--- iot_network/src/device.rs
+++ iot_network/src/device.rs
@@ -1,11 +1,15 @@
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 
+// Represents an IOT Device
 struct IotDevice {
     name:str,
+    location:Option<String>,
     ip:IPAddr,
     port:u8
 }
+
+
 
 
 
